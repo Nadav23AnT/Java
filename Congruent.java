85efa6c57c8f38991fd4bc3360d8ee66438a9c2a
Nadav Chen <nadav2282@gmail.com>
2020-11-08 12:14:47 +0200

    Edited Variables names

--- Congruent.java
+++ Congruent.java
@@ -12,62 +12,56 @@ public class Congruent
     public static void main (String[ ] args )
     {
         Scanner scan = new Scanner(System.in);
-        double X1, Y1, X2, Y2, X3, Y3;
+        double x1, y1, x2, y2, x3, y3;
         System.out.println("Please enter 3 coordinates of the first triangular:"); 
-        X1 = scan.nextDouble(); // Input x1 of his first triangular
-        Y1 = scan.nextDouble(); // Input y1 of his first triangular
-        X2 = scan.nextDouble(); // Input x2 of his first triangular
-        Y2 = scan.nextDouble(); // Input y2 of his first triangular
-        X3 = scan.nextDouble(); // Input x3 of his first triangular
-        Y3 = scan.nextDouble(); // Input y3 of his first triangular
+        x1 = scan.nextDouble(); // Input x1 of his first triangular
+        y1 = scan.nextDouble(); // Input y1 of his first triangular
+        x2 = scan.nextDouble(); // Input x2 of his first triangular
+        y2 = scan.nextDouble(); // Input y2 of his first triangular
+        x3 = scan.nextDouble(); // Input x3 of his first triangular
+        y3 = scan.nextDouble(); // Input y3 of his first triangular
 
-        double triangularRib1 =  Math.sqrt(Math.pow(X1-X2, 2) + Math.pow(Y1-Y2, 2)) / 2; 
+        double rib1 =  Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) / 2; 
         // will be the distance between the point 1 - 2
-        double triangularRib2 =  Math.sqrt(Math.pow(X2-X3, 2)+ 
-                Math.pow(Y2-Y3, 2)) / 2; 
+        double rib2 =  Math.sqrt(Math.pow(x2-x3, 2)+ Math.pow(y2-y3, 2)) / 2; 
         // will be the distance between the point 2 -3
-        double triangularRib3 =  Math.sqrt(Math.pow(X1-X3, 2) + Math.pow(Y1-Y3, 2)) / 2; 
+        double rib3 =  Math.sqrt(Math.pow(x1-x3, 2) + Math.pow(y1-y3, 2)) / 2; 
         // will be the distance between the point 3 - 1 
 
         System.out.println("Please enter 3 coordinates of the second triangular:"); 
-        double X4 = scan.nextDouble(); // Input x1 of his second triangular
-        double Y4 = scan.nextDouble(); // Input y1 of his second triangular
-        double X5 = scan.nextDouble(); // Input x2 of his second triangular
-        double Y5 = scan.nextDouble(); // Input y2 of his second triangular
-        double X6 = scan.nextDouble(); // Input x3 of his second triangular
-        double Y6 = scan.nextDouble(); // Input y3 of his second triangular
+        double x4 = scan.nextDouble(); // Input x1 of his second triangular
+        double y4 = scan.nextDouble(); // Input y1 of his second triangular
+        double x5 = scan.nextDouble(); // Input x2 of his second triangular
+        double y5 = scan.nextDouble(); // Input y2 of his second triangular
+        double x6 = scan.nextDouble(); // Input x3 of his second triangular
+        double y6 = scan.nextDouble(); // Input y3 of his second triangular
 
-        double triangularRib4 =  Math.abs(Math.sqrt(Math.pow(X4-X5, 2)+ 
-                    Math.pow(Y4-Y5, 2)) / 2); 
+        double rib4 =  Math.abs(Math.sqrt(Math.pow(x4-x5, 2)+ Math.pow(y4-y5, 2)) / 2); 
         // will be the distance between the point 1 - 2
-        double triangularRib5 =  Math.abs(Math.sqrt(Math.pow(X5-X6, 2)+ 
-                    Math.pow(-Y6, 2)) / 2); 
+        double rib5 =  Math.abs(Math.sqrt(Math.pow(x5-x6, 2)+ Math.pow(y5-y6, 2)) / 2); 
         // will be the distance between the point 2 -3
-        double triangularRib6 =  Math.abs(Math.sqrt(Math.pow(X6-X4, 2)+ 
-                    Math.pow(Y6-Y4, 2)) / 2);  
+        double rib6 =  Math.abs(Math.sqrt(Math.pow(x6-x4, 2)+ Math.pow(y6-y4, 2)) / 2);  
 
-        System.out.println("The first triangle is ("+X1+", "+ Y1+
-            ") ("+X2+", "+ Y2+") ("+X3+", "+ Y3+")");
+        System.out.println("The first triangle is ("+x1+", "+ y1+
+            ") ("+x2+", "+ y2+") ("+x3+", "+ y3+")");
         // Prints user coordinates
-        System.out.println("Its lengths are " + triangularRib1 +", "+
-            triangularRib2 +", "+ triangularRib3 +". ");
+        System.out.println("Its lengths are " + rib1 +", "+ rib2 +", "+ rib3 +". ");
         // Prints rib's lengths
 
-        System.out.println("The second triangle is ("+ X1+", "+ Y1+
-            ") ("+X2+", "+Y2+") ("+X3+", "+ Y3+")");
+        System.out.println("The second triangle is ("+ x1+", "+ y1+
+            ") ("+x2+", "+y2+") ("+x3+", "+ y3+")");
         // Prints user coordinates
-        System.out.println("Its lengths are " + triangularRib4+", "+ triangularRib5 +", "+
-            triangularRib6 +". ");
+        System.out.println("Its lengths are " + rib4+", "+ rib5 +", "+ rib6 +". ");
         // Prints all the info collected on triangular 2
-
-        if (triangularRib4-triangularRib1 == 0 & triangularRib5-triangularRib2 == 0){
-            // Comparing triangular 1 to triangular 2 by subtraction of each rib
-            if (triangularRib6-triangularRib3 == 0) // Subtraction the last rib
-                System.out.print("The triangles are congruent.");
+        
+        if (rib4 == rib1 || rib4 == rib2 || rib4 == rib3) // Campering rib4 to triangular 1 ribs
+            if (rib5 == rib1 || rib5 == rib2 || rib5 == rib3) // Campering rib5 to triangular 1 ribs
+                if(rib6 == rib1 || rib6 == rib2 || rib6 == rib3) // Campering rib6 to triangular 1 ribs
+                    System.out.print("The triangles are congruent."); // The triangles are congruent
+                else
+                    System.out.print("The triangles are not congruent.");
             else
-                System.out.print("The triangles are not congruent."); 
-        }
-        // The triangles are congruent
+                System.out.print("The triangles are not congruent.");
         else
             System.out.print("The triangles are not congruent.");
         // The triangles are not congruent
--- HeartRate.java
+++ HeartRate.java
@@ -10,7 +10,7 @@ public class HeartRate
 {
     public static void main (String[ ] args )
     {
-        final int CONST = 220; // CONST - age of the user = Suggested heart-rate
+        final int CONST = 220, HIGH = 85 , LOW = 65; // CONST - age of the user = Suggested heart-rate
         Scanner scan = new Scanner (System.in);
         System.out.println ("This program calculates your " +
             "target heart rate while exercising "); // Explanation about the program to the user
@@ -18,10 +18,10 @@ public class HeartRate
         System.out.print ("Enter your age: ");
         int age = scan.nextInt(); // User input his age
         
-        final int HIGH_LIMIT = (int)((CONST - age) * 0.85); // Calculate the high limit of the HeartReate
-        final int LOW_LIMIT = (int)((CONST - age) * 0.65); // Calculate the low limit of the HeartReate
+        int highLimit = (int)((CONST - age) * (HIGH/100)); // Calculate the high limit of the HeartReate
+        int lowLimit = (int)((CONST - age) * (LOW/100)); // Calculate the low limit of the HeartReate
         
-        System.out.println("Your estimated target heart rate zone is "+ LOW_LIMIT + "-" + HIGH_LIMIT + " beats per minute"); 
+        System.out.println("Your estimated target heart rate zone is "+ lowLimit + "-" + highLimit + " beats per minute"); 
         //Prints the output(result) to the user
 
     } // End of method main
